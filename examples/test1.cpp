7b71ec347de8fbafde7ab67dc5764e80ba61f8c9
ekojs999 <ekojs999@gmail.com>
2022-11-07 12:21:14 +0800

    add examples

---
+++ examples/test1.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <simpleTask.h>
+
+thread task1;
+
+bool sts;
+void flipFlop()
+{
+  digitalWrite(GPIO_NUM_2, sts);
+  sts = !(sts);
+}
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(GPIO_NUM_2, OUTPUT);
+  // init task delay every 1000 ms
+  task1.NewTask(1000);
+  // exc if time
+  task1.Exc(flipFlop);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
