51138a834801d560b6cf80147b07f495fb5d561a
eko johari s <ekojs999@gmail.com>
2022-11-11 07:05:53 +0800

    add main

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include <simpleTask.h>
+
+void tesInit()
+{
+    Serial.begin(9600);
+    pinMode(2, OUTPUT);
+    Serial.println("app task v1.1.0");
+    log("hallow %i\n", 100);
+}
+
+void tesBar(task *Arg)
+{
+    bool sts = Arg->getToggle();
+    digitalWrite(2, sts);
+}
+void tesRun2(task *Arg)
+{
+    log("hallow word");
+}
+
+int ac = 100;
+void tesRun(task *Arg)
+{
+    if (ac <= 0)
+    {
+        Arg->Exc(tesRun2);
+        Arg->setTimeMS(1000);
+        Arg->SetMode(TASK_MODE_ONESHOOT);
+
+        auto ts = Arg->GetTask("Bar");
+        ts->SetRunAfter(Arg->GetName());
+        ts->setEnable();
+    }
+    else
+    {
+        bool sts = Arg->getToggle();
+        digitalWrite(2, sts);
+        ac--;
+    }
+}
+
+thread t1, t2, t3;
+
+void setup()
+{
+    auto ts = t1.NewTask(1000, 5);
+    ts->SetName("Foo");
+    ts->SetMode(TASK_MODE_ONESHOOT);
+    ts->Exc(tesInit);
+
+    ts = t2.NewTask(200, 0);
+    ts->SetName("Flop");
+    ts->SetRunAfter("Foo");
+    ts->SetMode(TASK_MODE_DEFAULT);
+    ts->Exc(tesRun);
+
+    ts = t3.NewTask(1000, 5);
+    ts->SetName("Bar");
+    ts->SetMode(TASK_MODE_DEFAULT);
+    ts->Exc(tesBar);
+    ts->setDisable();
+}
+
+void loop()
+{
+    _TaskRunning();
+}
