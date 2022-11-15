aaed4e9e10af19cff213067f71ae1b10773e265d
eko johari s <ekojs999@gmail.com>
2022-11-16 01:14:29 +0800

    update new task

--- src/thread.cpp
+++ src/thread.cpp
@@ -12,38 +12,27 @@ void thread::searchID()
         }
     }
 }
-task *thread::NewTask(int delay)
-{
-    this->searchID();
-    if (this->ID != -1)
-    {
-        Task[this->ID].SetID(this->ID);
-        Task[this->ID].SetTask();
-        Task[this->ID].setTime(delay);
-    }
-    return this->GetTask();
-}
-task *thread::NewTask(int delay, uint8_t priority)
+task *thread::NewTask(uint8_t priority)
 {
     this->searchID();
     if (this->ID != -1)
     {
         this->GetTask()->SetPriority(priority);
         this->GetTask()->SetID(this->ID);
+        this->GetTask()->SetName("n_" + String(this->ID));
         this->GetTask()->SetTask();
-        this->GetTask()->setTime(delay);
     }
     return this->GetTask();
 }
-task *thread::NewTask(int delay, uint8_t priority, String name, task::HandlerFunc fn)
+task *thread::NewTask(uint8_t priority, String name, task::HandlerFunc fn)
 {
     this->searchID();
     if (this->ID != -1)
     {
         this->GetTask()->SetPriority(priority);
         this->GetTask()->SetID(this->ID);
+        this->GetTask()->SetName(name);
         this->GetTask()->SetTask();
-        this->GetTask()->setTime(delay);
         this->GetTask()->Exc(fn);
     }
     return this->GetTask();
